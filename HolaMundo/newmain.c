ccba342db9d60dae46e4611d1088c79dbf99b32d
gabbyta95 <gabbyta95@users.noreply.github.com>
2017-10-19 15:06:21 -0500

    Netbeans hola mundo

---
+++ HolaMundo/newmain.c
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   newmain.c
+ * Author: DANIELAROSERO
+ *
+ * Created on 18 de octubre de 2017, 20:09
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+printf("Hola mundo");
+    return (EXIT_SUCCESS);
+}
+
